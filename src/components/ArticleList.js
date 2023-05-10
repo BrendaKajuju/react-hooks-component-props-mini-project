9ecbaa051b058d0cd3a29193bbabbf49366de02a
Brenda <brendakajuju777@gmail.com>
2023-05-10 09:04:31 +0300

    Completed Assignment

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import Article from './Article';
 
-const ArticleList = (props) => {
-    const { posts } = props;
-    console.log (posts)
+const ArticleList = ({ posts }) => {
+    console.log(posts)
+    if (!posts || posts.length === 0) {
+        return <div>No posts to display</div>;
+      }
   return (
     <main>
-        {posts.map ((post) => (
-            <Article 
+    {posts.map((post) => (
+          <Article 
             key={post.id}
             title={post.title}
-            date={post.date}
+            formatDate={post.date}
             preview={post.preview}
             />
-        ))}
+    ))}    
     </main>
-    )
-}
+)}
 
 export default ArticleList
